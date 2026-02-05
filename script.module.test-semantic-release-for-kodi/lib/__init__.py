f252e28202e570d9b2bb9d618befba1f77564832
Brian Patrick Reavey <brian@reavey05.com>
2026-02-05 12:16:47 -0500

    feat: restructure as proper Kodi addon with dedicated directory

---
+++ script.module.test-semantic-release-for-kodi/lib/__init__.py
@@ -0,0 +1,12 @@
+# Test module for semantic release functionality
+"""
+This is a test module demonstrating automated semantic releases
+with conventional commits for Kodi addons.
+"""
+
+__version__ = "0.4.0"
+__author__ = "Test Author"
+
+def hello_world():
+    """A simple test function."""
+    return "Hello from semantic release test module!"
